c0b3857405947d443313cb875160d0828ad0448c
wouterlucas <wouter@wouterlucas.com>
2018-06-14 21:58:18 -0700

    Release

--- build/js/main.js
+++ build/js/main.js
@@ -108,7 +108,7 @@ conf = {
         } else if (bootStep === 4){
             console.debug('Rendering menu');
             plugins.menu = new Menu();
-            plugins.menu.render();
+            plugins.menu.render(activePlugin !== undefined ? activePlugin : conf.startPlugin);
             initNext();
         /*
          * Boot step 5 - render the default plugin from conf.startPlugin or the last visited from local
@@ -435,10 +435,6 @@ class Plugin {
                 this.socketListeners.splice(index, 1);
         };
 
-        activatePairing(callback) {
-            this.handleRequest('PUT', this.getURLStart('http') + 'Controller/PairingMode', null, callback);
-        };
-
         getSnapshotLocator(callback) {
             return this.getURLStart('http') + 'Snapshot/Capture?' + new Date().getTime();
         };
@@ -688,7 +684,7 @@ class Menu {
         this.nav.innerHTML = '';
     }
 
-    render() {
+    render(activePlugin) {
         this.clear();
 
         api.getControllerPlugins( (err, data) => {
@@ -708,11 +704,15 @@ class Menu {
                     console.debug('Menu :: rendering ' + plugin.callsign);
                     var li = document.createElement('li');
                     li.id = "item_" + plugin.callsign;
-                    if (i === 0) {
+
+                    if (activePlugin === undefined && i === 0) {
+                        li.className = 'menu-item active';
+                    } else if (activePlugin === plugin.callsign) {
                         li.className = 'menu-item active';
                     } else {
                         li.className = 'menu-item';
                     }
+
                     li.appendChild(document.createTextNode(plugins[ plugin.callsign ].displayName !== undefined ? plugins[ plugin.callsign ].displayName : plugin.callsign));
                     li.onclick = this.toggleMenuItem.bind(null, plugin.callsign);
                     ul.appendChild(li);
@@ -814,6 +814,177 @@ class Notifications {
     }
 
 }
+;/** The wifi plugin provides details on the available Wifi Adapters, scans for networks and allows the user to join networks through the UI
+ */
+
+class Bluetooth extends Plugin {
+
+    constructor(pluginData) {
+        super(pluginData);
+
+        this.devices = [];
+        this.displayName = 'Bluetooth';
+    }
+
+    render()        {
+        var mainDiv = document.getElementById('main');
+
+        mainDiv.innerHTML = `<div class="title grid__col grid__col--8-of-8">
+            Status
+        </div>
+
+        <div class="label grid__col grid__col--2-of-8">
+            Connected devices
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            <select id="BT_Devices"></select>
+        </div>
+
+        <div class="label grid__col grid__col--2-of-8"></div>
+        <div class="text grid__col grid__col--6-of-8">
+            <button type="button" id="BT_Connect">Connect</button>
+            <button type="button" id="BT_Disconnect">Disconnect</button>
+        </div>
+
+        <div class="title grid__col grid__col--8-of-8">
+            Discovery
+        </div>
+
+
+        <div class="label grid__col grid__col--2-of-8">
+            Discovered Devices
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            <select id="BT_DiscoveredDevices"></select>
+        </div>
+        <div class="label grid__col grid__col--2-of-8">
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            <button type="button" id="BT_ScanForDevices">Scan</button>
+            <button type="button" id="BT_Pair">Pair</button>
+        </div>
+
+        <br>
+        <div id="statusMessages" class="text grid__col grid__col--8-of-8"></div>
+        `;
+
+        // bind elements
+
+        // ---- button ----
+        this.scanButton                 = document.getElementById('BT_ScanForDevices');
+        this.pairButton                 = document.getElementById('BT_Connect');
+        this.connectButton              = document.getElementById('BT_Connect');
+        this.disconnectButton           = document.getElementById('BT_Disconnect');
+
+        //bind buttons
+        this.scanButton.onclick         = this.scanForDevices.bind(this);
+        this.pairButton.onclick         = this.pairDevice.bind(this);
+        this.disconnectButton.onclick   = this.disconnect.bind(this);
+        this.connectButton.onclick      = this.connect.bind(this);
+
+        // ---- Devices -----
+        this.devicesListEl              = document.getElementById('BT_Devices');
+        //this.devicesListEl.onchange     = this.renderDeviceDetails.bind(this);
+
+        // ---- Discovered Devices ----
+        this.discoveredDevicesEl        = document.getElementById('BT_DiscoveredDevices');
+        //this.discoveredDevicesEl.onchange = this.renderConfigDetails.bind(this);
+
+        this.update();
+    }
+
+    /* ----------------------------- DATA ------------------------------*/
+
+    update() {
+        api.getPluginData(this.callsign + '/ShowDeviceList', (err, resp) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+
+            // bail out if the plugin returns nothing
+            if (this.resp === undefined || this.resp.DeviceInfoList.length > 0)
+                return;
+
+            this.devices = this.resp.DeviceInfoList;
+            this.renderStatus();
+        });
+    }
+
+    /* ----------------------------- RENDERING ------------------------------*/
+
+    renderStatus () {
+        this.devicesListEl.innerHTML = '';
+        for (var i=0; i<this.devices.length; i++) {
+            var newChild = this.devicesListEl.appendChild(document.createElement("option"));
+            newChild.innerHTML = `${this.devices[i].name}`;
+        }
+    }
+
+    status(message) {
+        window.clearTimeout(this.statusMessageTimer);
+        this.statusMessages.innerHTML = message;
+
+        // clear after 5s
+        this.statusMessageTimer = setTimeout(this.status, 5000, '');
+    }
+
+    /* ----------------------------- BUTTONS ------------------------------*/
+
+    scanForDevices() {
+        var idx = this.devicesListEl.selectedIndex;
+        api.putPlugin(this.callsign, 'StartScan', null, (err, resp) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+
+            // get the results
+            // FIXME, there should be a seperate API to provide results on the "scanned results". For now go back to this.update
+            setTimeout(this.update.bind(this), 5000);
+        });
+    }
+
+    pairDevice() {
+        var idx = this.devicesListEl.selectedIndex;
+
+        this.status(`Pairing with ${this.devices[idx].name}`);
+
+        api.putPlugin(this.callsign, 'PairDevice', this,devices[idx].id, (err, resp) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+
+            // get the results
+            // FIXME, there should be a seperate API to provide results on the "scanned results"
+            setTimeout(this.update.bind(this), 5000);
+        });
+    }
+
+    connect() {
+        var idx = this.devicesListEl.selectedIndex;
+
+        this.status(`Connecting to ${this.devices[idx].name}`);
+
+        api.putPlugin(this.callsign, `Connect/${this.devices[idx].id}`, null, () =>{
+            // update after 5s
+            setTimeout(this.update.bind(this), 5000);
+        });
+    }
+
+    disconnect() {
+        var idx = this.devicesListEl.selectedIndex;
+
+        this.status(`Connecting to ${this.devices[idx].name}`);
+
+        api.deletePlugin(this.callsign, 'Connect/' + this.connected, null);
+    }
+
+}
+
+window.pluginClasses = window.pluginClasses || {};
+window.pluginClasses.Bluetooth = Bluetooth;
 ;/** The compositor plugin manages the Westeros compositor and its cliens through the webui
  */
 
@@ -1757,6 +1928,7 @@ class RemoteControl extends Plugin {
         this.displayName = 'Remote Control';
         this.onScreenKeyBoardIsRendered     = false;
         this.doNotHandleKeys                = false;
+        this.devicesThatSupportPairing      = ['GreenPeakRF4CE'];
 
         /**
          * Human to WPE key codes.
@@ -1972,8 +2144,13 @@ class RemoteControl extends Plugin {
         document.getElementById('main').innerHTML = '';
     }
 
-    activatePairing() {
-        api.activatePairing();
+    activatePairing(deviceName) {
+        api.putPlugin(this.callsign + '/' + deviceName, 'PairingMode', null, (err, resp) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+        });
     }
 
     render() {
@@ -1990,28 +2167,26 @@ class RemoteControl extends Plugin {
             -
           </div>
 
-          <div id="remoteControl_form"></div>`;
+          <div class="label grid__col grid__col--2-of-8">
+            Remotes
+          </div>
+          <div id="remotesList" class="text grid__col grid__col--6-of-8"></div>
+
+          <div id="pairingDiv"></div>
+          `;
 
         var self = this;
         api.getPluginData('RemoteControl', function (error, remotes) {
             if (remotes === undefined || remotes.devices === undefined)
                 return;
 
-            // if there is only 1 device, and its useless bail out
-            if (remotes.devices.length === 1 && remotes.devices[0].name === 'keymap')
+            // if there is only 1 keymap device, and its useless bail out
+            if (remotes.devices.length === 1 && remotes.devices[0] === 'keymap')
                 return;
 
             var devices = remotes.devices;
-            var form = document.getElementById('remoteControl_form');
-
-            // clearform
-            form.innerHTML = '';
-            var fieldset = document.createElement('fieldset');
-            form.appendChild(fieldset);
-
-            var legend = document.createElement('legend');
-            legend.innerHTML = 'Remotes';
-            fieldset.appendChild(legend);
+            var remotesDiv = document.getElementById('remotesList');
+            var pairingDiv = document.getElementById('pairingDiv');
 
             for (var i = 0; i < devices.length; i++) {
                 var device = devices[i];
@@ -2020,24 +2195,27 @@ class RemoteControl extends Plugin {
                 if (device === 'keymap')
                     continue;
 
-                var label = document.createElement('label');
-                label.innerHTML = device.name;
-                fieldset.appendChild(label);
+                remotesDiv.innerHTML += '' + device;
 
-                var p = document.createElement('p');
-                p.innerHTML = device.keys;
-                fieldset.appendChild(p);
+                if (i < devices.length-1)
+                    remotesDiv.innerHTML += ', ';
 
-                if (device.name == 'GreenPeakRF4CE') {
-                    var div = document.createElement('div');
-                    div.className = 'buttons';
-                    fieldset.appendChild(div);
+                if (self.devicesThatSupportPairing.indexOf(device) != -1) {
 
-                    var button = document.createElement('button');
-                    button.innerHTML = 'Pair remote';
-                    button.type = 'button';
-                    button.onclick = self.activatePairing.bind();
-                    div.appendChild(button);
+                    if (pairingDiv.innerHTML === '') {
+                        // add the title
+                        pairingDiv.innerHTML += `<div class="title grid__col grid__col--8-of-8">
+                          Pairing
+                        </div>`;
+                    }
+
+                    pairingDiv.innerHTML += `<div class="label grid__col grid__col--2-of-8">${device}</div>
+                        <div class="text grid__col grid__col--6-of-8">
+                            <button type="button" id="${device}-PairingMode">Enable Pairing</button>
+                        </div>`;
+
+                    var pairingButton = document.getElementById(device + '-PairingMode');
+                    pairingButton.onclick = self.activatePairing.bind(self, device);
                 }
             }
         });
