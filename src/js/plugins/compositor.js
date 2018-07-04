3b4b3a78a1983a861d9c1fc7375bc0abecdceb00
wouterlucas <wouter@wouterlucas.com>
2018-07-03 21:06:41 -0700

    Compositor: Only render controls if there is a client

--- src/js/plugins/compositor.js
+++ src/js/plugins/compositor.js
@@ -24,79 +24,22 @@ class Compositor extends Plugin {
         <div class="label grid__col grid__col--2-of-8">
             <label for="compositorClients">Clients</label>
         </div>
-        <div class="text grid__col grid__col--6-of-8">
+        <div class="text grid__col grid__col--6-of-8" id="compositorClientsDiv">
             <select id="compositorClients"></select>
         </div>
-        <div class="title grid__col grid__col--8-of-8">
-            Focus
-        </div>
-        <div class="text grid__col grid__col--6-of-8">
-            <button type="button" id="compositorSetTop">Set Top</button>
-            <button type="button" id="compositorSetInput">Set Input</button>
-        </div>
         <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
-            Opacity
-        </div>
-        <div class="text grid__col grid__col--3-of-8">
-            <div width=100px>
-                <input id="sliderOpacity" type="range" min="0" max="256" step="1" value="256"/>
-            </div>
-        </div>
-        <div class="text grid__col grid__col--1-of-8">
-            <input type="number" min="0" max="256" id="numOpacity" size="5" value="256"/>
-        </div>
-        <div class="text grid__col grid__col--6-of-8">
-            <button type="button" id="compositorSetOpacity">Set</button>
-        </div>
-        <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
-            Visibility
-        </div>
-        <div class="text grid__col grid__col--6-of-8">
-            <button type="button" id="webkit_hide">Hide</button>
-            <button type="button" id="webkit_show">Show</button>
-        </div>
-        <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
-            Geometry
-        </div>
-        <div class="text grid__col grid__col--2-of-8">
-            <input id="compositorXGeometry" type="number" value="0"/>
-        </div>
-        <div class="text grid__col grid__col--2-of-8">
-            <input id="compositorYGeometry" type="number" value="0"/>
-        </div>
-        <div class="text grid__col grid__col--2-of-8">
-            <input id="compositorWidthGeometry" type="number" value="1280" min="0"/>
-        </div>
-        <div class="text grid__col grid__col--2-of-8">
-            <input id="compositorHeightGeometry" type="number" value="720" min="0"/>
-        </div>
-        <div class="text grid__col grid__col--6-of-8">
-            <button type="button" id="compositorGeometry">Set</button>
-        </div>`;
+
+        <div id="controls"></div>`;
 
 
         // bind buttons & sliders
         document.getElementById('compositorResolutions').onclick     = this.setResolution.bind(this);
-        document.getElementById('compositorSetTop').onclick         = this.compositorAction.bind(this, 'Top');
-        document.getElementById('compositorSetInput').onclick       = this.compositorAction.bind(this, 'Input');
-        document.getElementById('sliderOpacity').onchange           = this.updateValue.bind(this, 'sliderOpacity', 'numOpacity');
-        document.getElementById('numOpacity').onchange              = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
-        document.getElementById('numOpacity').onkeyup               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
-        document.getElementById('numOpacity').onpaste               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
-        document.getElementById('numOpacity').oninput               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
-        document.getElementById('compositorSetOpacity').onclick     = this.compositorSetOpacity.bind(this);
-        document.getElementById('webkit_hide').onclick              = this.compositorVisible.bind(this, 'Hide');
-        document.getElementById('webkit_show').onclick              = this.compositorVisible.bind(this, 'Show');
-        document.getElementById('compositorGeometry').onclick       = this.compositorSetGeometry.bind(this);
-
 
         // bind fields
-        this.resolutionsList = document.getElementById('compositorResolutions');
-        this.menu = document.getElementById('compositorClients');
-
+        this.resolutionsList    = document.getElementById('compositorResolutions');
+        this.menu               = document.getElementById('compositorClients');
+        this.controlDiv         = document.getElementById('controls');
+        this.compositorClientsDiv = document.getElementById('compositorClientsDiv');
 
         // update resolutions field
         this.resolutionsList.innerHTML = '';
@@ -116,30 +59,99 @@ class Compositor extends Plugin {
             }
 
             // compositor is not returning anything, guess we're in client mode and not managing nxserver
-            if (resp === undefined || resp === null || resp.clients === undefined)
+            if (resp === undefined || resp === null || resp.clients === undefined) {
+                this.compositorClientsDiv.innerHTML = 'No clients found.';
                 return;
+            }
 
-            var clients = resp.clients;
-            if (clients.length > 0) {
-                var menu = document.getElementById('compositorClients');
-
-                menu.innerHTML = '';
-                var item = document.createElement('option');
-
-                item.value = '';
-                item.setAttributeNode(document.createAttribute('disabled'));
-                item.setAttributeNode(document.createAttribute('selected'));
-                item.innerHTML = 'Select a client';
-                menu.appendChild(item);
-
-                for (var i = 0; i < clients.length; i++) {
-                    var _item = document.createElement('option');
-                    _item.value = clients[i];
-                    _item.innerHTML = clients[i];
-                    menu.appendChild(_item);
-                }
+            if (resp.clients.length > 0) {
+                this.renderControls(resp.clients);
             }
         });
+    }
+
+    renderControls(clients) {
+        this.controlDiv.innerHTML = `<div class="title grid__col grid__col--8-of-8">
+            Controls
+        </div>        
+        <div class="text grid__col grid__col--2-of-8">
+            Focus
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            <button type="button" id="compositorSetTop">Set Top</button>
+            <button type="button" id="compositorSetInput">Set Input</button>
+        </div>
+        <div class="text grid__col grid__col--8-of-8"></div>
+        <div class="text grid__col grid__col--2-of-8">
+            Opacity
+        </div>
+        <div class="text grid__col grid__col--3-of-8">
+            <div width=100px>
+                <input id="sliderOpacity" type="range" min="0" max="256" step="1" value="256"/>
+            </div>
+        </div>
+        <div class="text grid__col grid__col--2-of-8">
+            <input type="number" min="0" max="256" id="numOpacity" size="5" value="256"/>
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <button type="button" id="compositorSetOpacity">Set</button>
+        </div>
+        <div class="text grid__col grid__col--8-of-8"></div>
+        <div class="text grid__col grid__col--2-of-8">
+            Visibility
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            <button type="button" id="webkit_hide">Hide</button>
+            <button type="button" id="webkit_show">Show</button>
+        </div>
+        <div class="text grid__col grid__col--8-of-8"></div>
+        <div class="text grid__col grid__col--2-of-8">
+            Geometry
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <input id="compositorXGeometry" type="number" value="0"/>
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <input id="compositorYGeometry" type="number" value="0"/>
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <input id="compositorWidthGeometry" type="number" value="1280" min="0"/>
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <input id="compositorHeightGeometry" type="number" value="720" min="0"/>
+        </div>
+        <div class="text grid__col grid__col--1-of-8">
+            <button type="button" id="compositorGeometry">Set</button>
+        </div>`;
+
+        document.getElementById('compositorSetTop').onclick         = this.compositorAction.bind(this, 'Top');
+        document.getElementById('compositorSetInput').onclick       = this.compositorAction.bind(this, 'Input');
+        document.getElementById('sliderOpacity').onchange           = this.updateValue.bind(this, 'sliderOpacity', 'numOpacity');
+        document.getElementById('numOpacity').onchange              = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
+        document.getElementById('numOpacity').onkeyup               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
+        document.getElementById('numOpacity').onpaste               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
+        document.getElementById('numOpacity').oninput               = this.updateValue.bind(this, 'numOpacity', 'sliderOpacity');
+        document.getElementById('compositorSetOpacity').onclick     = this.compositorSetOpacity.bind(this);
+        document.getElementById('webkit_hide').onclick              = this.compositorVisible.bind(this, 'Hide');
+        document.getElementById('webkit_show').onclick              = this.compositorVisible.bind(this, 'Show');
+        document.getElementById('compositorGeometry').onclick       = this.compositorSetGeometry.bind(this);
+
+        var menu = document.getElementById('compositorClients');
+        menu.innerHTML = '';
+        var item = document.createElement('option');
+
+        item.value = '';
+        item.setAttributeNode(document.createAttribute('disabled'));
+        item.setAttributeNode(document.createAttribute('selected'));
+        item.innerHTML = 'Select a client';
+        menu.appendChild(item);
+
+        for (var i = 0; i < clients.length; i++) {
+            var _item = document.createElement('option');
+            _item.value = clients[i];
+            _item.innerHTML = clients[i];
+            menu.appendChild(_item);
+        }
     }
 
     compositorAction(action) {
