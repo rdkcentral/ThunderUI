e28398918ba92e4f54b6430c7f705d11d4e033ca
wouterlucas <wouter@wouterlucas.com>
2018-07-03 21:07:12 -0700

    Release

--- build/js/main.js
+++ build/js/main.js
@@ -992,6 +992,8 @@ class Compositor extends Plugin {
 
     constructor(pluginData) {
         super(pluginData);
+
+        this.resolutions = ['720p', '720p50Hz', '1080p24Hz', '1080i50Hz', '1080p50Hz', '1080p60Hz'];
     }
 
     render()        {
@@ -1001,12 +1003,65 @@ class Compositor extends Plugin {
             Compositor
         </div>
         <div class="label grid__col grid__col--2-of-8">
-            <label for="compositorClients">Clients</label>
+            <label for="compositorResolutions">Resolution</label>
         </div>
         <div class="text grid__col grid__col--6-of-8">
+            <select id="compositorResolutions"></select>
+        </div>
+        <div class="label grid__col grid__col--2-of-8">
+            <label for="compositorClients">Clients</label>
+        </div>
+        <div class="text grid__col grid__col--6-of-8" id="compositorClientsDiv">
             <select id="compositorClients"></select>
         </div>
-        <div class="title grid__col grid__col--8-of-8">
+        <div class="text grid__col grid__col--8-of-8"></div>
+
+        <div id="controls"></div>`;
+
+
+        // bind buttons & sliders
+        document.getElementById('compositorResolutions').onclick     = this.setResolution.bind(this);
+
+        // bind fields
+        this.resolutionsList    = document.getElementById('compositorResolutions');
+        this.menu               = document.getElementById('compositorClients');
+        this.controlDiv         = document.getElementById('controls');
+        this.compositorClientsDiv = document.getElementById('compositorClientsDiv');
+
+        // update resolutions field
+        this.resolutionsList.innerHTML = '';
+
+        for (var i = 0; i < this.resolutions.length; i++) {
+            var _item = document.createElement('option');
+            _item.value = this.resolutions[i];
+            _item.innerHTML = this.resolutions[i];
+            this.resolutionsList.appendChild(_item);
+        }
+
+        // get clients
+        api.getPluginData(this.callsign, (err, resp) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+
+            // compositor is not returning anything, guess we're in client mode and not managing nxserver
+            if (resp === undefined || resp === null || resp.clients === undefined) {
+                this.compositorClientsDiv.innerHTML = 'No clients found.';
+                return;
+            }
+
+            if (resp.clients.length > 0) {
+                this.renderControls(resp.clients);
+            }
+        });
+    }
+
+    renderControls(clients) {
+        this.controlDiv.innerHTML = `<div class="title grid__col grid__col--8-of-8">
+            Controls
+        </div>        
+        <div class="text grid__col grid__col--2-of-8">
             Focus
         </div>
         <div class="text grid__col grid__col--6-of-8">
@@ -1014,7 +1069,7 @@ class Compositor extends Plugin {
             <button type="button" id="compositorSetInput">Set Input</button>
         </div>
         <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
+        <div class="text grid__col grid__col--2-of-8">
             Opacity
         </div>
         <div class="text grid__col grid__col--3-of-8">
@@ -1022,14 +1077,14 @@ class Compositor extends Plugin {
                 <input id="sliderOpacity" type="range" min="0" max="256" step="1" value="256"/>
             </div>
         </div>
-        <div class="text grid__col grid__col--1-of-8">
+        <div class="text grid__col grid__col--2-of-8">
             <input type="number" min="0" max="256" id="numOpacity" size="5" value="256"/>
         </div>
-        <div class="text grid__col grid__col--6-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <button type="button" id="compositorSetOpacity">Set</button>
         </div>
         <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
+        <div class="text grid__col grid__col--2-of-8">
             Visibility
         </div>
         <div class="text grid__col grid__col--6-of-8">
@@ -1037,27 +1092,25 @@ class Compositor extends Plugin {
             <button type="button" id="webkit_show">Show</button>
         </div>
         <div class="text grid__col grid__col--8-of-8"></div>
-        <div class="title grid__col grid__col--8-of-8">
+        <div class="text grid__col grid__col--2-of-8">
             Geometry
         </div>
-        <div class="text grid__col grid__col--2-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <input id="compositorXGeometry" type="number" value="0"/>
         </div>
-        <div class="text grid__col grid__col--2-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <input id="compositorYGeometry" type="number" value="0"/>
         </div>
-        <div class="text grid__col grid__col--2-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <input id="compositorWidthGeometry" type="number" value="1280" min="0"/>
         </div>
-        <div class="text grid__col grid__col--2-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <input id="compositorHeightGeometry" type="number" value="720" min="0"/>
         </div>
-        <div class="text grid__col grid__col--6-of-8">
+        <div class="text grid__col grid__col--1-of-8">
             <button type="button" id="compositorGeometry">Set</button>
         </div>`;
 
-
-        // bind buttons & sliders
         document.getElementById('compositorSetTop').onclick         = this.compositorAction.bind(this, 'Top');
         document.getElementById('compositorSetInput').onclick       = this.compositorAction.bind(this, 'Input');
         document.getElementById('sliderOpacity').onchange           = this.updateValue.bind(this, 'sliderOpacity', 'numOpacity');
@@ -1070,43 +1123,22 @@ class Compositor extends Plugin {
         document.getElementById('webkit_show').onclick              = this.compositorVisible.bind(this, 'Show');
         document.getElementById('compositorGeometry').onclick       = this.compositorSetGeometry.bind(this);
 
+        var menu = document.getElementById('compositorClients');
+        menu.innerHTML = '';
+        var item = document.createElement('option');
 
-        // bind fields
-        this.menu = document.getElementById('compositorClients');
+        item.value = '';
+        item.setAttributeNode(document.createAttribute('disabled'));
+        item.setAttributeNode(document.createAttribute('selected'));
+        item.innerHTML = 'Select a client';
+        menu.appendChild(item);
 
-        // get clients
-        api.getPluginData(this.callsign, (err, resp) => {
-            if (err !== null) {
-                console.error(err);
-                // HACK. Ignore error here, Bram thinks a 400 is OKAY. Parse or die trying
-                try {
-                    resp = JSON.parse(err);
-                } catch (e) {
-                    return;
-                }
-            }
-
-            var clients = resp.clients;
-            if (clients.length > 0) {
-                var menu = document.getElementById('compositorClients');
-
-                menu.innerHTML = '';
-                var item = document.createElement('option');
-
-                item.value = '';
-                item.setAttributeNode(document.createAttribute('disabled'));
-                item.setAttributeNode(document.createAttribute('selected'));
-                item.innerHTML = 'Select a client';
-                menu.appendChild(item);
-
-                for (var i = 0; i < clients.length; i++) {
-                    var _item = document.createElement('option');
-                    _item.value = clients[i];
-                    _item.innerHTML = clients[i];
-                    menu.appendChild(_item);
-                }
-            }
-        });
+        for (var i = 0; i < clients.length; i++) {
+            var _item = document.createElement('option');
+            _item.value = clients[i];
+            _item.innerHTML = clients[i];
+            menu.appendChild(_item);
+        }
     }
 
     compositorAction(action) {
@@ -1149,7 +1181,15 @@ class Compositor extends Plugin {
             if (err)
                 console.error(err);
         });
+    }
 
+    setResolution() {
+        var _res = this.resolutionsList.options[this.resolutionsList.selectedIndex].value;
+
+        api.postPlugin(this.callsign, 'Resolution/' + _res, null, (err, resp) => {
+            if (err)
+                console.error(err);
+        });
     }
 
 }
@@ -1840,6 +1880,58 @@ class Netflix extends Plugin {
 
 window.pluginClasses = window.pluginClasses || {};
 window.pluginClasses.Netflix = Netflix;
+;/** The ocdm plugin manages different OpenCDM DRM modules
+ */
+
+class OCDM extends Plugin {
+
+    constructor(pluginData) {
+        super(pluginData);
+
+        this.ocdmTemplate = `<div class="label grid__col grid__col--2-of-8">
+            {{Name}}
+        </div>
+        <div class="text grid__col grid__col--6-of-8">
+            {{Designators}}
+        </div>`;
+    }
+
+    render()        {
+        var mainDiv = document.getElementById('main');
+
+        mainDiv.innerHTML = `<div class="title grid__col grid__col--8-of-8">
+            OpenCDM Systems
+        </div>
+        <div id="systemDiv"></div>`;
+
+        this.systemDiv = document.getElementById('systemDiv');
+        this.update();
+    }
+
+    update() {
+        api.getPluginData(this.callsign, (err, resp) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            if (resp === undefined || resp === null || resp.systems === undefined)
+                return;
+
+
+            for (var i=0; i<resp.systems.length; i++) {
+                var system = resp.systems[i];
+
+                var systemElement = this.ocdmTemplate.replace('{{Name}}', system.name);
+                systemElement = systemElement.replace('{{Designators}}', system.designators.toString());
+                this.systemDiv.innerHTML += systemElement;
+            }
+        });
+    }
+}
+
+window.pluginClasses = window.pluginClasses || {};
+window.pluginClasses.OCDM = OCDM;
 ;/** The provision plugin checks the device identifier and is able to initiate a provisioning request if not provisioned
  */
 
