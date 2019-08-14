a31f26c19ec5546940e6d374d7a89579cc4d50a3
Michiel van der Geest <hello@michielvandergeest.com>
2019-08-14 11:15:40 -0300

    Added local build location for development (besides dist).

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const CopyPlugin = require('copy-webpack-plugin')
 const Dotenv = require('dotenv-webpack');
+const path = require('path')
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
+        path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? 'dist' : 'local',)
     },
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devtool: process.env.NODE_ENV === 'production' ? '' : 'source-map',
