9874fb5e7c85971c695db93cf7089d9c09d4858b
Michiel van der Geest <hello@michielvandergeest.com>
2019-08-27 06:35:45 -0300

    Moved resolveHostName function to separate helper file and call it _before_ initing the application.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './css/style.css'
 import './css/toast.css'
 
 import { init } from './js/core/application.js'
+import { resolveHostnamePort } from './js/helpers.js'
 
-init(process.env.HOST)
+init(process.env.HOST || resolveHostnamePort())
